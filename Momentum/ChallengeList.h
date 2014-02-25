27ae28b2d832bcf5cc62e2d47c3bd647a856e64c
AppGroup <Joe@Joes-MacBook-Pro.local>
2014-02-25 22:25:11 +1300

    Update

--- Momentum/ChallengeList.h
+++ Momentum/ChallengeList.h
@@ -11,6 +11,14 @@
 
 /**
  *  ChallengeList is a view controller that shows the user the current list of challenges.
+    Within it, there are 3 main aspects:
+                    - the table view
+                    - the name of the challenge
+                    - information on the challenge
+ 
+    Data is stored in coredata, and every time the ChallengeList is entered it checks for 
+    any updates by sending a list of its stored challenges to the server. If the server 
+    has any new challenges, it will return these after the custom api call.
  */
 
 @interface ChallengeList : UIViewController <UITableViewDataSource, UITableViewDelegate>
--- Momentum/ChallengeList.m
+++ Momentum/ChallengeList.m
@@ -15,25 +15,29 @@
 #import "CharityListView.h"
 
 @interface ChallengeList ()
-//define iboutlets
+
+//main challenge list
 @property (strong, nonatomic) IBOutlet UITableView *challengeList;
-@property (weak, nonatomic) IBOutlet UIImageView *bottomImage;
-@property (weak, nonatomic) IBOutlet UIButton *acceptChallengeButton;
-@property (weak, nonatomic) IBOutlet UITextView *infoTextView;
-@property (weak, nonatomic) IBOutlet UILabel *challengeNameLabel;
+
+//image views for the sponsor logos and bottom images
 @property (weak, nonatomic) IBOutlet UIImageView *sponsorLogo;
+@property (weak, nonatomic) IBOutlet UIImageView *bottomImage;
+
+//these describe the challenge
+@property (weak, nonatomic) IBOutlet UILabel *challengeNameLabel;
+@property (weak, nonatomic) IBOutlet UITextView *infoTextView;
+
+@property (weak, nonatomic) IBOutlet UIButton *acceptChallengeButton;
 
 @property (strong, nonatomic) NSArray* challengeArray;
 
 @property NSInteger numberOfRows;
-@property NSOperationQueue *lazyLoad;
+@property NSOperationQueue *imgDownloadQueue;
 
 @property (strong, nonatomic) AuthService* authService;
 @property (strong, nonatomic) CoreDataSingleton *coreData;
-
 @property (strong, nonatomic) SoloChallenge* currentChallenge;
 
-@property (strong, nonatomic) UIView *dimView;
 @property (strong, nonatomic) UIActivityIndicatorView *loadingView;
 
 - (IBAction)AcceptChallenge:(id)sender;
@@ -43,6 +47,10 @@
 @implementation ChallengeList
 
 
+/**
+ *  ChallengeList's implementation of view did load deals with several factors.
+     - 
+ */
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -50,7 +58,7 @@
     //hide the default ui button
     self.navigationItem.hidesBackButton=YES;
     
-    //set the new back button
+    //creates a new back button to replace the navigation controller one
     UIButton* backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40, 30)];
     [backButton setTitle:@"Back" forState:UIControlStateNormal];
     [backButton setContentEdgeInsets:UIEdgeInsetsMake(-2.5, 0, 0, 0)];
@@ -61,6 +69,7 @@
     UIBarButtonItem* backBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backButton];
     [self.navigationItem setLeftBarButtonItem:backBarButtonItem];
     
+    /**challenge list set up**/
     self.challengeList.delegate = self;
     self.challengeList.dataSource = self;
     
@@ -69,9 +78,6 @@
     self.challengeList.showsVerticalScrollIndicator = NO;
     self.challengeList.transform = CGAffineTransformMakeRotation(-M_PI * 0.5);
    
-    //remove annoying touch content delay
-    self.infoTextView.delaysContentTouches = NO;
-    
     //add a border around the button
     [self.acceptChallengeButton.layer setBorderColor: [[UIColor whiteColor] CGColor]];
     [self.acceptChallengeButton.layer setBorderWidth: 1.0];
@@ -80,41 +86,17 @@
     self.authService = [AuthService getInstance];
     self.coreData = [[CoreDataSingleton alloc] init];
     
-    //[self.coreData deleteAllEntitiesOfType:@"SoloWalkingChallenge"];
+    [self.coreData deleteAllEntitiesOfType:@"SoloWalkingChallenge"];
     //[self.coreData deleteSpecificChallenge:@"SoloWalkingChallenge"];
     
-
+    //if at some point, img data has been malformed, remove this data from the database.
+    //will only really need to be done if user exits the app while imaegs are downloading
     [self.coreData purgeBrokenChallenges];
     
-    [self getChallengeData];
+    [self setChallengesFromCoreData];
 
     [self downloadChallengeData];
     
-}
-
--(NSDictionary *) getStoredChallenges{
-    //get the list of challenges that are stored in coredata, we send this
-    //data to the service to check for updates
-    NSArray *challenges = [self.coreData fetchEntitiesOfType:@"SoloWalkingChallenge"];
-    NSMutableArray *challengeNames = [NSMutableArray new];
-    
-    for(int i = 0; i < challenges.count; i++){
-        SoloChallenge *s = challenges[i];
-        [challengeNames addObject:s.challengeName];
-    }
-    
-    //dictionary containing all the stored challenges
-    //to send to server
-    NSDictionary *storedChallenges = [[NSDictionary alloc] initWithObjectsAndKeys:challengeNames,@"challengenames", nil];
-    
-    //if there are no stored challenges 
-    if(challengeNames.count == 0){
-        self.loadingView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-        self.loadingView.center = CGPointMake(160, 250);
-        [self.view addSubview:self.loadingView];
-        [self.loadingView startAnimating];
-    }
-    return storedChallenges;
 }
 
 
@@ -134,8 +116,7 @@
             NSMutableArray* rows = [NSMutableArray new];
         
             //an async task to fetch data from the server
-            self.lazyLoad = [[NSOperationQueue alloc] init];
-            self.lazyLoad.name = @"loadImagesFromServer";
+            self.imgDownloadQueue = [[NSOperationQueue alloc] init];
             
             for (int i = 0; i < challenges.count; i++) {
                 //add to the list of index paths for the tableview insert row method,
@@ -152,45 +133,7 @@
                 //increment number of rows for the table view
                 self.numberOfRows++;
                 
-                //asynchronous fetch for the images
-                [self.lazyLoad addOperationWithBlock:^{
-                    //image for the challenge
-                    NSURL *url = [NSURL URLWithString:[challenges[i] valueForKey:@"sChallengeImage"]];
-                    NSData *data = [NSData dataWithContentsOfURL:url];
-                    
-                    //image for the bottom image
-                    NSURL *urlBottom = [NSURL URLWithString:[challenges[i] valueForKey:@"sChallengeImageBtm"]];
-                    NSData *dataBottom = [NSData dataWithContentsOfURL:urlBottom];
-                    
-                    //get the cell to assign
-                    ChallengeCell *cell = (ChallengeCell *)[self.challengeList cellForRowAtIndexPath:path];
-                    
-                    //update the ui
-                    [[NSOperationQueue mainQueue] addOperationWithBlock: ^ {
-                        self.challengeList.backgroundColor = [UIColor blackColor];
-                        
-                        //set this here as we want to fade in the images
-                        cell.imageBackground.alpha = 0;
-                        cell.bottomImage = [UIImage imageWithData:dataBottom];
-                        
-                        cell.imageBackground.image = [UIImage imageWithData:data];
-                        
-                        //set the data for the image in core data
-                        [self.coreData addChallengeImageData:data andBottomImageData:dataBottom forChallenge:[challenges[i] valueForKey:@"sChallengeName"]];
-                        
-                        //set the background image
-                        if([self.challengeList indexPathForSelectedRow].row == path.row){
-                            self.bottomImage.image = [UIImage imageWithData:dataBottom];
-                            //bottom image alpha 0 for fade in
-                            self.bottomImage.alpha = 0;
-                        }
-                        
-                        [UIView animateWithDuration:0.3 animations:^{
-                            cell.imageBackground.alpha = 1;
-                            self.bottomImage.alpha = 1;
-                        }];
-                    }];
-                }];
+                [self downloadAndSetChallengeImages:challengeData withChallengePath:path];
             }
             
             //refresh the challenge array to
@@ -211,9 +154,78 @@
     }];    
 }
 
-//fetches challenge data from core data. The data is assigned in
-//cellForIndexPath
-- (void) getChallengeData{
+- (NSDictionary *) getStoredChallenges{
+    //get the list of challenges that are stored in coredata, we send this
+    //data to the service to check for updates
+    NSArray *challenges = [self.coreData fetchEntitiesOfType:@"SoloWalkingChallenge"];
+    NSMutableArray *challengeNames = [NSMutableArray new];
+    
+    for(int i = 0; i < challenges.count; i++){
+        SoloChallenge *s = challenges[i];
+        [challengeNames addObject:s.challengeName];
+    }
+    
+    //dictionary containing all the stored challenges
+    //to send to server
+    NSDictionary *storedChallenges = [[NSDictionary alloc] initWithObjectsAndKeys:challengeNames,@"challengenames", nil];
+    
+    //if there are no stored challenges we need a loading
+    //gif, so add this to the view/plain white background
+    if(challengeNames.count == 0){
+        self.loadingView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        self.loadingView.center = CGPointMake(160, 250);
+        [self.view addSubview:self.loadingView];
+        [self.loadingView startAnimating];
+    }
+    
+    return storedChallenges;
+}
+
+-(void) downloadAndSetChallengeImages:(NSDictionary *) challenge withChallengePath:(NSIndexPath*) path{
+    //asynchronous fetch for the images
+    [self.imgDownloadQueue addOperationWithBlock:^{
+        //image for the challenge
+        NSURL *url = [NSURL URLWithString:[challenge valueForKey:@"sChallengeImage"]];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        
+        //image for the bottom image
+        NSURL *urlBottom = [NSURL URLWithString:[challenge valueForKey:@"sChallengeImageBtm"]];
+        NSData *dataBottom = [NSData dataWithContentsOfURL:urlBottom];
+        
+        //get the cell to assign
+        ChallengeCell *cell = (ChallengeCell *)[self.challengeList cellForRowAtIndexPath:path];
+        
+        //update the ui
+        [[NSOperationQueue mainQueue] addOperationWithBlock: ^ {
+            self.challengeList.backgroundColor = [UIColor blackColor];
+            
+            //set this here as we want to fade in the images
+            cell.imageBackground.alpha = 0;
+            cell.bottomImage = [UIImage imageWithData:dataBottom];
+            
+            cell.imageBackground.image = [UIImage imageWithData:data];
+            
+            //set the data for the image in core data
+            [self.coreData addChallengeImageData:data andBottomImageData:dataBottom forChallenge:[challenge valueForKey:@"sChallengeName"]];
+            
+            //set the background image
+            if([self.challengeList indexPathForSelectedRow].row == path.row){
+                self.bottomImage.image = [UIImage imageWithData:dataBottom];
+                //bottom image alpha 0 for fade in
+                self.bottomImage.alpha = 0;
+            }
+            
+            [UIView animateWithDuration:0.3 animations:^{
+                cell.imageBackground.alpha = 1;
+                self.bottomImage.alpha = 1;
+            }];
+            
+        }];
+    }];
+}
+
+
+- (void) setChallengesFromCoreData{
     //update the number of rows currently stored in coredata
     self.challengeArray = [self.coreData fetchEntitiesOfType:@"SoloWalkingChallenge"];
     self.numberOfRows = self.challengeArray.count;
@@ -230,6 +242,7 @@
                            animated:YES
                      scrollPosition:UITableViewScrollPositionMiddle];
 
+    //set the appropriate information by the cell that has been selected
     ChallengeCell *cell = (ChallengeCell*)[tableView cellForRowAtIndexPath:indexPath];
     self.infoTextView.text = cell.information;
     self.infoTextView.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:13.0];
